ef5ec3e20e993d9e13ee57855e04b7d3a8f3e0e2
jandrwiega <60856871+jandrwiega@users.noreply.github.com>
2020-05-02 18:22:09 +0200

    officialrealise01.05

--- js/offer.js
+++ js/offer.js
@@ -2,18 +2,26 @@ const kmpiheader = document.querySelector(".systemkmpi--header");
 const kmpiall = document.querySelector(".systemkmpi");
 const kmpibackground = document.querySelector(".systemkmpi--background-photo");
 const kmpitext = document.querySelector(".systemkmpi--text");
+const kmpibutton = document.querySelector(".systemkmpi--button-btn");
+const kmpibuttonhref = document.querySelector(".systemkmpi--button-a");
 const headercolorswitch = ()=>
 {
-    kmpiheader.style.color="mediumaquamarine";
+    kmpiheader.style.color="darkcyan";
     kmpiheader.style.transform="scale(1.05)";
-    kmpibackground.style.opacity="0.4";
+    kmpiall.style.opacity="0.8";
+    kmpibutton.style.backgroundcolor="darkcyan";
+    kmpibutton.style.transform="scale(1.03)";
+    kmpibuttonhref.style.color="white";
 }
 
 const headerdefaultcolor = ()=>
 {
-    kmpiheader.style.color="#222222";
+    kmpiheader.style.color="mediumaquamarine";
     kmpiheader.style.transform="scale(1)";
-    kmpibackground.style.opacity="0.6";
+    kmpiall.style.opacity="1";
+    kmpibutton.style.backgroundcolor="mediumaquamarine";
+    kmpibutton.style.transform="scale(1)";
+    kmpibuttonhref.style.color="#222222";
 }
 
 kmpiall.addEventListener("mouseover",headercolorswitch);
